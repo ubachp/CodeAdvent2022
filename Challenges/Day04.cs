f55e70a472617c4e35da638101136b5b83d63b06
Paulo Ubach <paulo.ubach@gmail.com>
2022-12-04 10:11:09 -0500

    Day 4 challenge

--- Challenges/Day04.cs
+++ Challenges/Day04.cs
@@ -1,5 +1,4 @@
 ﻿using CodeAdvent2022.Shared;
-using System.Text;
 
 namespace CodeAdvent2022.Challenges
 {
@@ -11,11 +10,35 @@ namespace CodeAdvent2022.Challenges
 
         public async Task Solve()
         {
-            //string input = await Helper.GetInput(@"https://adventofcode.com/2022/day/4/input", Cookie);
-            //var inputArray = input.Split("\n");
-            
+            string input = await Helper.GetInput(@"https://adventofcode.com/2022/day/4/input", Cookie);
+            var inputArray = input.Split("\n");
+            var totalPairs = 0;
+            var totalOverlap = 0;
 
-            Helper.PrintResults("", 0, "", 0);
+            foreach (var line in inputArray)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                var pair = line.Split(',');
+
+                var firstRange = pair[0].Split("-");
+                var secondRange = pair[1].Split("-");
+
+                var firstRangeStart = int.Parse(firstRange[0]);
+                var firstRangeEnd = int.Parse(firstRange[1]);
+                var secondRangeStart = int.Parse(secondRange[0]);
+                var seconRangeEnd = int.Parse(secondRange[1]);
+
+                var firstRangeList = Enumerable.Range(firstRangeStart, firstRangeEnd - firstRangeStart + 1);
+                var secondRangeList = Enumerable.Range(secondRangeStart, seconRangeEnd - secondRangeStart + 1);
+
+                var intersectionCount1 = firstRangeList.Intersect(secondRangeList).Count();
+                var intersectionCount2 = secondRangeList.Intersect(firstRangeList).Count();
+
+                totalPairs += intersectionCount1 == firstRangeList.Count() || intersectionCount2 == secondRangeList.Count() ? 1 : 0;
+                totalOverlap += intersectionCount1 > 0 || intersectionCount2 > 0 ? 1 : 0;
+            }
+
+            Helper.PrintResults("total pairs", totalPairs, "total overlap", totalOverlap);
         }
     }
 }
